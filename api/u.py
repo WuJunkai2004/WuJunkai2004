ea0b0a526d9e498294e7fc8b7f2f9d0a760e9e6f
Wu Junkai <wu.junkai@qq.com>
2022-03-06 09:19:45 +0800

    Add files via upload

---
+++ api/u.py
@@ -0,0 +1,40 @@
+try:
+    import requests
+except ImportError:
+    import urllib.request as request
+
+import ssl
+context = ssl._create_unverified_context()
+
+try:
+    import vercel
+except:
+    tag = 'date'
+else:
+    tag = ''
+
+def get(tag):
+    url = 'https://wu-junkai.vercel.app/api/'+tag
+    try:
+        # https://blog.csdn.net/chitaozi/article/details/106381353
+        return request.urlopen(url,context=context).read().decode('utf-8')
+    except BaseException as e:
+        return 'Net Error :\n' + ' '.join(map(str,list(e.args)))
+
+class handle(vercel.API):
+    def vercel(self, url, data, headers):
+        self.send_code(200)
+
+        if(tag):
+            try:
+                tag = url.split('#')[1]
+            except IndexError:
+                tag = 'date'
+
+        self.send_text('get https://wu-junkai.vercel.app/api/'+tag+'\n'+
+                       '\n'+
+                       'result =====\n'+
+                       '\n'+
+                       get(tag)
+                       )
+
