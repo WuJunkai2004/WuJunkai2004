4d07914598df99a680f49294e7423acc23a474ce
Wu Junkai <wu.junkai@qq.com>
2022-03-13 21:56:39 +0800

    Add files via upload

---
+++ mongo/insert.py
@@ -0,0 +1,34 @@
+import json
+import time
+
+import requests
+import vercel
+
+
+class handler(vercel.API):
+    def vercel(self, url, data, headers):
+        if(not data):
+            self.send_code(200)
+            self.send_text(json.dumps({"upload":"fail","error":"Need data"}))
+            return
+        url = "https://data.mongodb-api.com/app/data-uqbpx/endpoint/data/beta/action/insertOne"
+        payload = json.dumps({
+            "collection": "student",
+            "database": "class7",
+            "dataSource": "Cluster0",
+            "document": data
+        })
+        headers = {
+            'Content-Type': 'application/json',
+            'Access-Control-Request-Headers': '*',
+            'api-key': 'eOYzarNsIRJ7h9n0JEWGqa59yMKN6i8u1temFAmjRDz3qLzDIfAHRFX8A8ESPwnx'
+        }
+        while(True):
+            try:
+                response = requests.post(url, headers=headers, data=payload)
+            except:
+                time.sleep(0.1)
+            else:
+                break
+        self.send_code(200)
+        self.send_text(response.text)
